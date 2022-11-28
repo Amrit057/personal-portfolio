dc95d553d5318ff785ea8df793259934a2560d68
Amrit Pandey <pandeyamrit714@gmail.com>
2022-11-28 12:57:37 +0545

    validate

--- components/sections/ContactForm.js
+++ components/sections/ContactForm.js
@@ -1,29 +1,34 @@
+import {useForm} from "react-hook-form"
+
 export default function ContactForm() {
-    // async function handleOnSubmit(e) {
-    //     e.preventDefault();
-    //     const formData = {}
-    //     Array.from(e.currentTarget.elements).forEach(field => {
-    //         if (!field.name) return;
-    //         formData[field.name] = field.value;
-    //     });
-    //     fetch('api/mail', {
-    //         method: 'post',
-    //         body: JSON.stringify(formData)
-    //     })
-    //     console.log(formData)
-    // }
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm();
+
+    const handle = () => {
+        alert("Submitted");
+    }
     return (
-        <div className="p-10 bg-green-100 bg-no-repeat bg-contain flex justify-center" id="contact">
+        <div className="p-10 bg-green-100 bg-no-repeat bg-contain flex flex-col items-center justify-center" id="contact">
+            <h1 className="font-extrabold text-4xl mb-5">Contact</h1>
             <div>
-                <h1 className="font-extrabold text-4xl mb-5">Contact</h1>
-                <form name="contact" method="POST" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field" className="flex flex-col gap-2">
+                <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit(handle)} data-netlify-honeypot="bot-field" className="flex flex-col gap-2">
                     <input name="bot-field" type="hidden"/>
+
                     <label for="name">Name:</label>
-                    <input className="outline-0 w-96" type="text" id="name" name="name"/>
+                    <input className="outline-0 w-96" {...register('name', {required: true})}/>
+                    {errors.name && <p className="text-red-700">Please enter your name.</p>}
+
                     <label for="email">Email:</label>
-                    <input className="outline-0 w-96" type="email" id="name" name="email" />
+                    <input className="outline-0 w-96" {...register('email', {pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/}, {required: true})}/>
+                    {errors.email && <p className="text-red-700">Please enter valid email.</p> }
+
                     <label for="message">Message:</label>
-                    <textarea className="outline-0 w-96" rows={5} id="message" name="message" />
+                    <textarea className="outline-0 w-96" rows={5} {...register('message', {required: true})}/>
+                    {errors.message && <p className="text-red-700">Please enter your message.</p>}
+
                     <button className="bg-slate-400 rounded-full px-2 py-1 w-20" type="submit">Submit</button>
                 </form>
             </div>
