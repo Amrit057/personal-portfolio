af170a264616059058f16695ce1b1d2f0a96d212
Amrit Pandey <pandeyamrit714@gmail.com>
2022-11-28 10:29:04 +0545

    update

--- components/sections/ContactForm.js
+++ components/sections/ContactForm.js
@@ -1,28 +1,29 @@
 export default function ContactForm() {
-    async function handleOnSubmit(e) {
-        e.preventDefault();
-        const formData = {}
-        Array.from(e.currentTarget.elements).forEach(field => {
-            if (!field.name) return;
-            formData[field.name] = field.value;
-        });
-        fetch('api/mail', {
-            method: 'post',
-            body: JSON.stringify(formData)
-        })
-        // console.log(formData)
-    }
+    // async function handleOnSubmit(e) {
+    //     e.preventDefault();
+    //     const formData = {}
+    //     Array.from(e.currentTarget.elements).forEach(field => {
+    //         if (!field.name) return;
+    //         formData[field.name] = field.value;
+    //     });
+    //     fetch('api/mail', {
+    //         method: 'post',
+    //         body: JSON.stringify(formData)
+    //     })
+    //     console.log(formData)
+    // }
     return (
-        <div className="p-10 bg-contact bg-green-100 bg-no-repeat bg-contain flex justify-center">
+        <div className="p-10 bg-green-100 bg-no-repeat bg-contain flex justify-center">
             <div>
                 <h1 className="font-extrabold text-4xl mb-5">Contact</h1>
-                <form method="post" onSubmit={handleOnSubmit} className="flex flex-col gap-2">
-                    <label htmlFor="name">Name:</label>
-                    <input className="outline-0 w-96" type="text" name="name" />
-                    <label htmlFor="email">Email:</label>
-                    <input className="outline-0 w-96" type="email" name="email" />
-                    <label htmlFor="message">Message:</label>
-                    <textarea className="outline-0 w-96" rows={5} name="message" />
+                <form name="contact" method="POST" data-netlify="true" onSubmit="submit" data-netlify-honeypot="bot-field" className="flex flex-col gap-2">
+                    <input name="bot-field" type="hidden"/>
+                    <label for="name">Name:</label>
+                    <input className="outline-0 w-96" type="text" id="name" name="name" />
+                    <label for="email">Email:</label>
+                    <input className="outline-0 w-96" type="email" id="name" name="email" />
+                    <label for="message">Message:</label>
+                    <textarea className="outline-0 w-96" rows={5} id="message" name="message" />
                     <button className="bg-slate-400 rounded-full px-2 py-1 w-20" type="submit">Submit</button>
                 </form>
             </div>
