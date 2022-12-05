c2ed262611b4cad9eb2e1b88a88c74242dc25dcb
Amrit Pandey <pandeyamrit714@gmail.com>
2022-12-05 17:16:27 +0545

    update

--- components/sections/Banner.js
+++ components/sections/Banner.js
@@ -1,11 +1,13 @@
 import Typewriter from "typewriter-effect"
+import Link from "next/link"
+import { BsFacebook, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 export default function Banner() {
     return (
         <div className="h-screen flex items-center justify-center border-b-2 border-gray-600" id="banner">
             <div>
                 <p className="text-5xl text-center">Hi! I am Amrit Pandey.</p>
-                <div className="text-center sm:text-left">
+                <div className="text-center mb-3 sm:text-left">
                     <Typewriter
                         options={{
                             autoStart: true,
@@ -17,6 +19,11 @@ export default function Banner() {
                             ],
                         }}
                     />
+                </div>
+                <div className="flex gap-2 justify-center sm:justify-start">
+                    <Link href='https://www.facebook.com/' target='_blank'><BsFacebook /></Link>
+                    <Link href='https://www.instagram.com/' target='_blank'><BsInstagram /></Link>
+                    <Link href='https://www.linkedin.com/' target='_blank'><BsLinkedin /></Link>
                 </div>
             </div>
         </div>
