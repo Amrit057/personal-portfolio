72951bc8cccabac6362040116026895235ad5a9f
Amrit Pandey <pandeyamrit714@gmail.com>
2022-12-05 17:28:30 +0545

    update

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     const {darkMode} = useContext(DarkModeContext);
 
     return (
-        <div className={`text-center p-4 border-t-2 border-gray-600 ${ darkMode ? 'bg-gray-900 text-white' : 'bg-gray-400' }`}>
+        <div className={`text-center p-4 border-t-2 border-gray-600 ${ darkMode ? 'bg-gray-900 text-white' : 'bg-slate-500' }`}>
             <p>Copyright ©️ 2022. All rights reserved.</p>
         </div>
     )
